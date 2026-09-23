e1808f73386a97cc69cc163a892ae4f4fa3daa08
Subbu <Subbudevediga608@gmail.com>
2026-09-23 22:42:10 +0530

    Fix test after failure simulation

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,7 +8,7 @@ class TestReleaseManagementApp(unittest.TestCase):
         result = get_release_status()
         self.assertEqual(
             result,
-            "This test shoud fail"
+            "Release Management CI/CD Project is running"
         )
 
 
